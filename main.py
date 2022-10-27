902dbd888a60980c419088930e0c616b1d124738
Jairo Garcia <jairogarcia@Jairos-MacBook-Pro.local>
2022-10-26 21:17:05 -0700

    defined slot-m spin function

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
+
+import random
+
+
 # Global Variables
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A" : 2,
+    "B" : 4,
+    "C" : 6,
+    "D" : 8
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbol.items():
+        for i in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = [[],[],[]]
 
 def deposit():
     while True:
-        amount = input("What would you like to deposit? $")
+        amount = input("What would you like to deposit? $ ")
         if amount.isdigit():
             amount = int(amount)
             if amount > 0:
@@ -28,11 +49,33 @@ def get_number_of_lines():
     return lines
 
 def get_bet():
+    while True:
+        amount = input("What would you like to bet on each line? $ ")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}.")
+        else:
+            print("Please enter a number.")
+    return amount
+
 
 
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    print(balance, lines)
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f"You do not have enough to bet that amount, your current balance is: ${balance}")
+        else:
+            break
+
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: {total_bet}.")
+
 
 main()
