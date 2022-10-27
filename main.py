c826bc912995392082e86b335d604b8dc13754ae
Jairo Garcia <jairogarcia@Jairos-MacBook-Pro.local>
2022-10-27 08:19:13 -0700

    def printing slots

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python3
 import random
 
 
@@ -10,19 +10,36 @@ ROWS = 3
 COLS = 3
 
 symbol_count = {
-    "A" : 2,
-    "B" : 4,
-    "C" : 6,
-    "D" : 8
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
 }
 
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
-    for symbol, symbol_count in symbol.items():
+    for symbol, symbol_count in symbols.items():
         for i in range(symbol_count):
             all_symbols.append(symbol)
 
     columns = [[],[],[]]
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(all_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+        columns.append(column)
+    return columns
+
+def print_slot_machine():
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], "|")
+            else:
+                print(column[row])
 
 def deposit():
     while True:
@@ -77,5 +94,7 @@ def main():
 
     print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: {total_bet}.")
 
+    slots = get_slot_machine_spin(ROWS,COLS,symbol_count)
+    print_slot_machine(slots)
 
 main()
