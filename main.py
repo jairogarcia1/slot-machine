8010d67148e2c241d02aa05045e60f96b957f7a3
Jairo Garcia <jairogarcia@Jairos-MacBook-Pro.local>
2022-10-26 20:42:07 -0700

    declaring max and min bets

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# Global Variables
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
 
 def deposit():
     while True:
@@ -11,4 +15,24 @@ def deposit():
 
     return amount
 
-deposit()
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number of lines to bet on(1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines.")
+
+    return lines
+
+def get_bet():
+
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    print(balance, lines)
+
+main()
